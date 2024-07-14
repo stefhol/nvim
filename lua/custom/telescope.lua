cf8d220239ee8802b02de4ad19640ad334453a68
stefhol <hoef.stef@pm.me>
2024-07-14 23:20:14 +0200

    fix: remove deprecated function

--- lua/custom/telescope.lua
+++ lua/custom/telescope.lua
@@ -1,14 +1,14 @@
 local data = assert(vim.fn.stdpath "data") --[[@as string]]
-local trouble = require "trouble.providers.telescope"
+local trouble = require "trouble.sources.telescope"
 require("telescope").setup {
   defaults = {
     mappings = {
       i = {
-        ["<c-t>"] = trouble.open_with_trouble,
+        ["<c-t>"] = trouble.open,
         ["<C-u>"] = false,
         ["<C-d>"] = false,
       },
-      n = { ["<c-t>"] = trouble.open_with_trouble },
+      n = { ["<c-t>"] = trouble.open },
     },
   },
   extensions = {
